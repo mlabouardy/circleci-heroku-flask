8c1ce08461ddd2c291e057fae17f90f13285919e
mlabouardy <mohamed@labouardy.com>
2017-10-09 15:05:57 +0200

    install heroku cli

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -3,6 +3,10 @@
 set -eu
 
 git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
